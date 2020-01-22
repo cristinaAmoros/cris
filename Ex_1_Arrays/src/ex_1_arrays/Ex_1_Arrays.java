2c16228772df4b5febeecb85d83e0a763621aa15
cristinaAmoros <craamfa@gmail.com>
2020-01-22 13:53:50 +0100

    primera pujada

---
+++ Ex_1_Arrays/src/ex_1_arrays/Ex_1_Arrays.java
@@ -0,0 +1,19 @@
+
+package ex_1_arrays;
+
+
+public class Ex_1_Arrays {
+
+  
+    public static void main(String[] args) {
+        // cristina amoros
+        int[] vector = new int[10]; 
+        int i;
+        for(i=0; i<=4; i++)
+        {
+        System.out.println(vector[i]);
+
+    }
+    
+}
+}
